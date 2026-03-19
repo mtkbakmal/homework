378b6b9e256324a378930bd09260d0daba0c34f9
Matkurbanov Akmal <matkurb.akmal@icloud.com>
2026-03-19 16:09:11 +0600

    Add files via upload

---
+++ numsquarescubes.py
@@ -0,0 +1,12 @@
+numbers = []
+squares = []
+cubes = []
+
+for i in range(1,11):
+    numbers.append(i)
+    squares.append(i**2)
+    cubes.append(i**3)
+
+print("Числа:", numbers)
+print("Квадраты:", squares)
+print("Кубы:", cubes)
